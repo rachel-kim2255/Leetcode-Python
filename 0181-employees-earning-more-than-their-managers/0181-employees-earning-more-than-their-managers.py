765cea6e03fc5d46cd5b9720fc3d8bd75b580f53
Rachel Kim <rachelkim2255@gmail.com>
2025-10-14 22:32:32 -0700

    Time: 278 ms (88.47%), Space: 68.5 MB (37.67%) - LeetHub

---
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
+    merged = employee.merge(employee, left_on = 'managerId', right_on ="id", suffixes=('', '_manager'))
+    result = merged[merged['salary'] > merged['salary_manager']][['name']]
+    result.rename(columns={'name': 'Employee'}, inplace=True)
+    return result
+
+
