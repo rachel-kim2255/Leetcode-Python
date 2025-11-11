eaced6e9f5b29e4c2e346918facfbb52d74b8d3c
Rachel Kim <rachelkim2255@gmail.com>
2025-11-10 16:44:32 -0800

    Time: 307 ms (42.91%), Space: 67.8 MB (67.96%) - LeetHub

---
+++ 0197-rising-temperature/0197-rising-temperature.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
+    weather = weather.sort_values('recordDate')
+    weather['recordDate'] = pd.to_datetime(weather['recordDate'])
+    weather['prev_temp'] = weather['temperature'].shift(1)
+    weather['prev_date'] = weather['recordDate'].shift(1)
+
+    result = weather[
+        ((weather['recordDate'] - weather['prev_date']).dt.days == 1) &
+        (weather['temperature'] > weather['prev_temp'])
+    ][['id']]
+
+    return result
