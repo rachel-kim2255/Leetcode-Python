15a53e122577d6d2531f3fcf5d20490001329c78
Rachel Kim <rachelkim2255@gmail.com>
2025-11-12 16:20:21 -0800

    Time: 248 ms (74.63%), Space: 66.7 MB (25.77%) - LeetHub

--- 0627-swap-sex-of-employees/0627-swap-sex-of-employees.py
+++ 0627-swap-sex-of-employees/0627-swap-sex-of-employees.py
@@ -1,6 +1,15 @@
+# KEY: replace
+
 import pandas as pd
 
 def swap_salary(salary: pd.DataFrame) -> pd.DataFrame:
     salary['sex'] = salary['sex'].replace({'m': 'f', 'f':'m'})
     return salary
+
+    # 2. lambda funciton
+    # salary['sex'] = salary['sex'].apply(lambda x: 'f' if x == 'm' else 'm')
+    # return salary
+
+
+
     
