4593603df37ff767a747c22e28b28dd17e98f0f5
Rachel Kim <rachelkim2255@gmail.com>
2025-11-12 16:09:39 -0800

    Time: 294 ms (14.25%), Space: 67.2 MB (66.17%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.py
+++ 0620-not-boring-movies/0620-not-boring-movies.py
@@ -4,7 +4,6 @@ def not_boring_movies(cinema: pd.DataFrame) -> pd.DataFrame:
     cinema = cinema[cinema['description'] != 'boring']
     cinema = cinema[cinema['id'] % 2 == 1]
     cinema = cinema.sort_values(by = 'rating', ascending= False)
-    cinema = pd.DataFrame(cinema)
     return cinema
     
     
