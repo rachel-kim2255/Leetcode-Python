96abe2732de0ac08b1c49fa5f91f7952f3769cd9
Rachel Kim <rachelkim2255@gmail.com>
2025-12-01 23:05:50 -0800

    Time: 295 ms (51.53%), Space: 67.4 MB (51.7%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.py
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.py
@@ -4,3 +4,7 @@ def find_products(products: pd.DataFrame) -> pd.DataFrame:
     products = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
     return products[['product_id']]
     
+    # 2. Method Chaining
+    # return products[
+    #     (products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')
+    # ][['product_id']]
