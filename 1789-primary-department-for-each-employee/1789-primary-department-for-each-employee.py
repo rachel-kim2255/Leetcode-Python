7bf80463aa6892a1db782d1e547338b70abb1157
Rachel Kim <rachelkim2255@gmail.com>
2025-12-01 23:25:57 -0800

    Time: 347 ms (29.9%), Space: 67.2 MB (84.67%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.py
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.py
@@ -1,8 +1,17 @@
+# import pandas as pd
+
+# def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
+#     result = employee[employee['primary_flag'] == 'Y']
+#     count_id = employee['employee_id'].value_counts()
+#     result2 = employee[employee['employee_id'].map(count_id) == 1]
+#     result_df = pd.concat([result, result2], ignore_index=True)
+#     return result_df[['employee_id', 'department_id']]
+
 import pandas as pd
 
 def find_primary_department(employee: pd.DataFrame) -> pd.DataFrame:
-    result = employee[employee['primary_flag'] == 'Y']
-    count_id = employee['employee_id'].value_counts()
-    result2 = employee[employee['employee_id'].map(count_id) == 1]
-    result_df = pd.concat([result, result2], ignore_index=True)
-    return result_df[['employee_id', 'department_id']]
+    dept_counts = employee.groupby('employee_id')['department_id'].transform('count')
+    final = (employee['primary_flag'] == 'Y') | (dept_counts == 1)
+    result = employee[final]
+    
+    return result[['employee_id', 'department_id']]
