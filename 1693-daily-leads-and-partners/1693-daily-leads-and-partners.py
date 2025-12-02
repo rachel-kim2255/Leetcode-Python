947f09cdde99c7503a5f6754060f5f6b45437e0e
Rachel Kim <rachelkim2255@gmail.com>
2025-12-01 19:52:34 -0800

    Time: 393 ms (18.58%), Space: 68.3 MB (24.59%) - LeetHub

--- 1693-daily-leads-and-partners/1693-daily-leads-and-partners.py
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
     
-    counting = daily_sales.groupby(['date_id', 'make_name'],as_index=False).nunique(['lead_id', 'partner_id'])
+    counting = daily_sales.groupby(['date_id', 'make_name'])[['lead_id', 'partner_id']].nunique().reset_index()
     counting= counting.rename(columns={'lead_id':'unique_leads', 'partner_id':'unique_partners'})
     
     return counting
-    
+
