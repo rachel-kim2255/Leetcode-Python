1db7021615f0dbf82e4975641d311a70f1816e6c
Rachel Kim <rachelkim2255@gmail.com>
2025-12-02 23:36:14 -0800

    Time: 291 ms (91.47%), Space: 67.7 MB (65.53%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.py
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -1,6 +1,7 @@
 # LEARN: numpy - np.where / lambda - apply()
 import pandas as pd
 
+# 1. lambda, apply()
 # def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
 #     employees['bonus'] = employees.apply(
 #         lambda row: row['salary']
@@ -10,8 +11,8 @@ import pandas as pd
 #     )
 #     return employees[['employee_id', 'bonus']].sort_values('employee_id')
 
+# 2. numpy, np.where
 import numpy as np
-
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
     condition = (employees['employee_id'] % 2 != 0) & (employees['name'].str[0] != 'M')
     employees['bonus'] = np.where(
