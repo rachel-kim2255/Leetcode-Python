eead25f28b5cd40cef81869291af7136140e4b82
Rachel Kim <rachelkim2255@gmail.com>
2025-12-02 23:35:26 -0800

    Time: 308 ms (65.63%), Space: 68 MB (24.27%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.py
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.py
@@ -1,11 +1,23 @@
+# LEARN: numpy - np.where / lambda - apply()
 import pandas as pd
 
+# def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+#     employees['bonus'] = employees.apply(
+#         lambda row: row['salary']
+#                 if(row['employee_id'] % 2 != 0 and row['name'][0] != 'M')
+#                 else 0,
+#         axis =1
+#     )
+#     return employees[['employee_id', 'bonus']].sort_values('employee_id')
+
+import numpy as np
+
 def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['bonus'] = employees.apply(
-        lambda row: row['salary']
-                if(row['employee_id'] % 2 != 0 and row['name'][0] != 'M')
-                else 0,
-        axis =1
+    condition = (employees['employee_id'] % 2 != 0) & (employees['name'].str[0] != 'M')
+    employees['bonus'] = np.where(
+        condition,
+        employees['salary'],
+        0
     )
-    return employees[['employee_id', 'bonus']].sort_values('employee_id')
-    
+
+    return employees[['employee_id', 'bonus']].sort_values(by='employee_id')
