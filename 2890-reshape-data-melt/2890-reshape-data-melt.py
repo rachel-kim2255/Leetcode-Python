7deb6f0d6fa6179e88ee45fe577994022cab120f
Rachel Kim <rachelkim2255@gmail.com>
2025-12-05 00:43:43 -0800

    Time: 291 ms (79.7%), Space: 66.5 MB (78.04%) - LeetHub

--- 2890-reshape-data-melt/2890-reshape-data-melt.py
+++ 2890-reshape-data-melt/2890-reshape-data-melt.py
@@ -4,7 +4,6 @@ def meltTable(report: pd.DataFrame) -> pd.DataFrame:
     df = pd.melt(
         report,
         id_vars= ['product'],
-        value_vars = ['quarter_1','quarter_2','quarter_3','quarter_4'],
         var_name = 'quarter',
         value_name = 'sales'
     )
