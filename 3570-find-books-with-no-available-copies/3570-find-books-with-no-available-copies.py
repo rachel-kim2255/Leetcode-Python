ccad336e07dc3ecbaf43077c9052ba69d301799b
Rachel Kim <rachelkim2255@gmail.com>
2025-12-09 23:23:17 -0800

    Time: 294 ms (62.05%), Space: 68.1 MB (82.56%) - LeetHub

--- 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.py
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.py
@@ -1,28 +1,32 @@
 import pandas as pd
 
 def find_books_with_no_available_copies(library_books: pd.DataFrame, borrowing_records: pd.DataFrame) -> pd.DataFrame:
+    total_borrowed = borrowing_records[borrowing_records['return_date'].isna()].groupby(['book_id']).size().reset_index(name='current_borrowers')
+    merged = library_books.merge(total_borrowed, on='book_id', how='left')
+    result = merged[merged['total_copies'] == merged['current_borrowers']]
+    return result.drop(columns='total_copies').sort_values(by=['current_borrowers', 'title'], ascending=[False, True])
 
-    df1 = library_books
-    df2 = borrowing_records
+    # df1 = library_books
+    # df2 = borrowing_records
     
-    loans = df2[df2['return_date'].isna()]
-    counts = loans.groupby('book_id').size().reset_index(name='current_borrowers')
+    # loans = df2[df2['return_date'].isna()]
+    # counts = loans.groupby('book_id').size().reset_index(name='current_borrowers')
 
-    df = df1.merge(
-        counts, 
-        on='book_id', 
-        how='left'
-    )
+    # df = df1.merge(
+    #     counts, 
+    #     on='book_id', 
+    #     how='left'
+    # )
     
-    df['current_borrowers'] = df['current_borrowers'].fillna(0)
-    df['available_copies'] = df['total_copies'] - df['current_borrowers']
-    df = df[df['available_copies'] == 0].copy()
-    final_cols = ['book_id', 'title', 'author', 'genre', 'publication_year', 'current_borrowers']
+    # df['current_borrowers'] = df['current_borrowers'].fillna(0)
+    # df['available_copies'] = df['total_copies'] - df['current_borrowers']
+    # df = df[df['available_copies'] == 0].copy()
+    # final_cols = ['book_id', 'title', 'author', 'genre', 'publication_year', 'current_borrowers']
     
-    return df[final_cols].sort_values(
-        by=['current_borrowers', 'title'],
-        ascending=[False, True]
-    )
+    # return df[final_cols].sort_values(
+    #     by=['current_borrowers', 'title'],
+    #     ascending=[False, True]
+    # )
 
 
 
