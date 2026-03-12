e8ad3bbc7e5b1d673dcfc4bcf41f04736dad36d9
Rachel Kim <rachelkim2255@gmail.com>
2026-03-11 22:11:46 -0700

    Time: 316 ms (26.02%), Space: 68.4 MB (70.37%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
-    merge = employee.merge(employee, how='inner', left_on = 'managerId', right_on='id', suffixes=['_emp', '_mgr'])
-    filtered = merge[merge['salary_emp'] > merge['salary_mgr']]
-    return filtered.rename(columns={'name_emp':'Employee'})[['Employee']]
+    merge = employee.merge(employee, how='inner', left_on='managerId', right_on='id', 
+            suffixes=['_emp', '_mgr'])
+    filtering = merge[merge['salary_emp'] > merge['salary_mgr']]
+    return filtering.rename(columns={'name_emp':'Employee'})[['Employee']]
 
